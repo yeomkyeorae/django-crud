270eaa689a9e6a305f92bb09ab3d44b3153ac4ec
yeomkyeorae <duarufp06@naver.com>
2019-09-10 17:09:58 +0900

    11 | modified URL by adding app_name & used Decorator on function of views

--- articles/views.py
+++ articles/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render, redirect
+# POST 요청만 허용할 수 있도록 'require_POST'를 import, 아래 delete에서 사용
+from django.views.decorators.http import require_POST
+# embed를 사용하면 embed() 함수에서 실행이 멈추고 IPython이 열려 현재까지의 변수 내용을 출력해 볼 수 있음.
+from IPython import embed
+
 from .models import Article
+
 
 # Create your views here.
 def index(request):
@@ -7,24 +13,22 @@ def index(request):
     context = {
         'articles': articles,
     }
+    # embed()
     return render(request, 'articles/index.html', context)
 
 
-def new(request):
-    return render(request, 'articles/new.html')
-
-
 def create(request):
-    title = request.POST.get('title')
-    content = request.POST.get('content')
-    article = Article(title=title, content=content)
-    article.save()
-    # context = {
-    #     'number': len(Article.objects.all()),
-    # }
+    # request의 방식이 'GET'이면
+    if request.method == 'GET':
+        return render(request, 'articles/new.html')
+    else:
+        title = request.POST.get('title')
+        content = request.POST.get('content')
+        article = Article(title=title, content=content)
+        article.save()
+        # app_name: articles의 name이 detail인 path
+        return redirect('articles:detail', article.pk)
 
-    # return render(request, 'articles/create.html', context)
-    return redirect('articles:detail', article.pk)
 
 
 def detail(request, article_pk):
@@ -35,26 +39,30 @@ def detail(request, article_pk):
 
     return render(request, 'articles/detail.html', context)
 
-
+# POST 요청만 받아들이기 위한 decorator
+@require_POST
 def delete(request, article_pk):
     article = Article.objects.get(pk=article_pk)
     article.delete()
-
+    # app_name: articles의 name이 index인 path
     return redirect('articles:index')
+    # else:
+    #     return redirect('articles:detail', article.pk)
 
 
 def update(request, article_pk):
     article = Article.objects.get(pk=article_pk) 
-    context = {
-        'article': article,
-        'article_pk': article_pk,
-    }
-    return render(request, 'articles/update.html', context)
+    # 요청 방식이 'GET'이면
+    if request.method == 'GET':
+        context = {
+            'article': article,
+            'article_pk': article_pk,
+        }
 
+        return render(request, 'articles/update.html', context)
+    else:
+        article.title = request.POST.get('title')
+        article.content = request.POST.get('content')
+        article.save()
 
-def updated(request, article_pk):
-    article = Article.objects.get(pk=article_pk)
-    article.title = request.POST.get('title')
-    article.content = request.POST.get('content')
-    article.save()
-    return render(request, 'articles/updated.html')
+        return render(request, 'articles/updated.html')
