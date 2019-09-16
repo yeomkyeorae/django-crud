bf23850c61f16a85a105a8843e27a114b1bfce64
yeomkyeorae <duarufp06@naver.com>
2019-09-16 10:43:44 +0900

    12 | Create Comment(1:N) Model

--- articles/admin.py
+++ articles/admin.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Article
+from .models import Article, Comment
 
 class ArticleAdmin(admin.ModelAdmin):
     list_display = ('id', 'title', 'content', 'created_at', 'updated_at')
 
+
+class CommentAdmin(admin.ModelAdmin):
+    list_displat = ('id', 'content', 'article_id')
+
+
 admin.site.register(Article, ArticleAdmin)
+admin.site.register(Comment, CommentAdmin)
